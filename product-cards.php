8e407042ac3403785a8885aa4f4f5ad29440d414
vargabyan <vardan.gabrielyan.it@gmail.com>
2023-12-30 18:26:46 +0500

    #6

--- product-cards.php
+++ product-cards.php
@@ -5,12 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+    />
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <title>карточки товара</title>
 </head>
 
 <body>
 
-<main class="container">
+<div class="container">
     <section class="product-cards_header-wrapper">
         <h1 class="product-cards_header">Сайдинг hokla Ю-пласт 2,0м Color Брусника</h1>
         <p class="product-cards_view"><span>91</span> человек смотрит сейчас</p>
@@ -36,24 +41,44 @@
 
     <section class="product-cards">
         <div class="product-cards_gallery-wrap">
-
+            <div class="product-cards_gallery_showcase">
+                <img src="images/image%20374.png" alt="">
+            </div>
+            <div class="product-cards_gallery_items-wrapper">
+                <div class="product-cards_gallery_item">
+                    <img src="images/image%20374.png" alt="">
+                </div>
+                <div class="product-cards_gallery_item">
+                    <img src="images/image%20376.png" alt="">
+                </div>
+                <div class="product-cards_gallery_item">
+                    <img src="images/image%20404.png" alt="">
+                </div>
+            </div>
         </div>
         <div class="product-cards_characteristic-wrapper">
+            <p class="product-cards_vendor-code">Артикул товара: <span>24500000</span></p>
             <p class="product-cards_characteristic_header">Характеристики</p>
             <div class="product-cards_characteristic">
-                <p class="product-cards_characteristic_item">Длина <span>2000</span></p>
-                <p class="product-cards_characteristic_item">Ширина <span>250 мм</span></p>
+                <p class="product-cards_characteristic_item">Длина <span
+                            class="product-cards_characteristic_item-line"></span> <span
+                            class="product-cards_characteristic_item-price">2000</span></p>
+                <p class="product-cards_characteristic_item">Ширина <span
+                            class="product-cards_characteristic_item-line"></span> <span
+                            class="product-cards_characteristic_item-price">250 мм</span></p>
             </div>
             <a class="product-cards_characteristic_view-all-specifications" href="">Посмотреть все характеристики</a>
         </div>
         <div class="product-cards_shipping-and-payment">
             <div class="product-cards_shipping-and-payment_item">
                 <div class="product-cards_shipping-and-payment_icon"></div>
-                <p class="product-cards_shipping-and-payment_text">Бесплатная доставка по Екатеринбургу при заказе от 30 000 рублей</p>
+                <p class="product-cards_shipping-and-payment_text">Бесплатная доставка по Екатеринбургу при заказе от 30
+                    000 рублей</p>
             </div>
             <div class="product-cards_shipping-and-payment_item">
                 <div class="product-cards_shipping-and-payment_icon"></div>
-                <p class="product-cards_shipping-and-payment_text">Оплата наличными при получении или удобным для вас способом. Безналичный расчет для организаций и частных лиц</p>
+                <p class="product-cards_shipping-and-payment_text">Оплата наличными при получении или удобным для вас
+                    способом. Безналичный расчет для организаций и частных лиц</p>
             </div>
         </div>
         <div class="product-cards_price-and-purchase">
@@ -66,17 +91,255 @@
         </div>
     </section>
 
-    
-    <section>
-        <h2>Гарантия лучшей цены</h2>
-        <p>Мы даем лучшие цены, потому что мы работаем напрямую с производителями</p>
-        
-        <div>
-            <div></div>
-            <div></div>
+
+    <section class="best-price-guarantee-wrapper">
+        <h2 class="best-price-guarantee_header">Гарантия лучшей цены</h2>
+        <p class="best-price-guarantee_sub-title">Мы даем лучшие цены, потому что мы работаем напрямую с
+            производителями</p>
+
+        <div class="best-price-guarantee">
+            <div class="best-price-guarantee_price-from-others-wrapper">
+                <div class="price-from-others_head">
+                    <div class="price-from-others_head_grid-line"><p class="price-from-others_head_item">Компания</p>
+                    </div>
+                    <div class="price-from-others_head_grid-line"><p class="price-from-others_head_item">Их цена</p>
+                    </div>
+                    <div class="price-from-others_head_grid-line"><p class="price-from-others_head_item">Выгода</p>
+                    </div>
+                </div>
+                <div class="best-price-guarantee_price-from-others">
+                    <img class="price-from-others_item_logo" src="images/image%20405.png" alt="">
+                    <p class="price-from-others_item_text">430.1 р.</p>
+                    <p class="price-from-others_item_text">39.1 р.</p>
+                </div>
+                <div class="best-price-guarantee_price-from-others">
+                    <img class="price-from-others_item_logo" src="images/image%20407.png" alt="">
+                    <p class="price-from-others_item_text">449.65 р.</p>
+                    <p class="price-from-others_item_text">58.65 р.</p>
+                </div>
+                <div class="best-price-guarantee_price-from-others">
+                    <img class="price-from-others_item_logo" src="images/image%20408.png" alt="">
+                    <p class="price-from-others_item_text">625.6 р.</p>
+                    <p class="price-from-others_item_text">234.6 р.</p>
+                </div>
+                <div class="best-price-guarantee_price-from-others">
+                    <img class="price-from-others_item_logo" src="images/image%20409.png" alt="">
+                    <p class="price-from-others_item_text">625.6 р.</p>
+                    <p class="price-from-others_item_text">234.6 р.</p>
+                </div>
+                <div class="best-price-guarantee_bottom_sub-title">
+                    <p class="best-price-guarantee_bottom_sub-title_item">*Цены не являются публичной аффертой</p>
+                    <p class="best-price-guarantee_bottom_sub-title_item">**Все данные берутся с сайтов компаний</p>
+                </div>
+            </div>
+            <div class="best-price-guarantee_our-price">
+                <div class="best-price-guarantee_price-wrapper">
+                    <p class="product-cards_price-and-purchase_price">Наша цена<span>391.00 руб.</span></p>
+                    <p class="product-cards_price-and-purchase_manufacturer">Производитель: <span>Ю-пласт</span></p>
+                </div>
+                <p class="product-cards_price-and-purchase_discount">535 руб.<span>Скидка 37%</span></p>
+                <p class="product-cards_price-and-purchase_in-stock">В наличии</p>
+                <div class="best-price-guarantee_btn-wrapper">
+                    <button class="product-cards_price-and-purchase_add-to-basket btn-red">Добавить в корзину</button>
+                    <button class="product-cards_price-and-purchase_Buy-in-a-click btn-grey">Купить в 1 клик</button>
+                </div>
+            </div>
         </div>
     </section>
-</main>
+</div>
+
+<section class="description-and-specifications_background">
+    <div class="description-and-specifications_line"></div>
+    <div class="description-and-specifications container" data-description-and-specifications-wrapper>
+        <div class="description-and-specifications_toggle-btn-wrapper">
+            <div class="description-and-specifications_toggle" data-description-and-specifications-toggle-description>
+                <p class="description-and-specifications_toggle-btn">Описание</p>
+            </div>
+            <div class="description-and-specifications_toggle"
+                 data-description-and-specifications-toggle-specifications>
+                <p class="description-and-specifications_toggle-btn active">Технические характеристики</p>
+            </div>
+        </div>
+        <div class="description-and-specifications_content-wrapper">
+            <div class="description-and-specifications_description" data-description-and-specifications-description>
+                <p class="description-and-specifications_description_item">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias architecto, aspernatur cupiditate
+                    dignissimos doloremque dolorum ducimus eaque excepturi fuga fugiat hic iusto maiores neque numquam
+                    placeat quisquam ratione veniam voluptatem? A aperiam architecto consequatur deleniti eius excepturi
+                    quisquam sapiente veritatis. Architecto dicta dolore dolores, enim et ex excepturi fugit hic
+                    inventore iusto, laborum libero, numquam obcaecati odio omnis sunt voluptates!
+                </p>
+            </div>
+            <div class="description-and-specifications_specifications active"
+                 data-description-and-specifications-specifications>
+                <?php for ($i = 0; $i < 5; $i++) { ?>
+                    <div class="description-and-specifications_specifications_item">
+                        <p class="description-and-specifications_specifications_item_text">
+                            Тип
+                            <span class="description-and-specifications_specifications_item_line"></span>
+                            <span class="description-and-specifications_specifications_item_value">Монолитный поликарбонат</span>
+                        </p>
+                        <p class="description-and-specifications_specifications_item_text">
+                            Длина, см
+                            <span class="description-and-specifications_specifications_item_line"></span>
+                            <span class="description-and-specifications_specifications_item_value">300</span>
+                        </p>
+                        <p class="description-and-specifications_specifications_item_text">
+                            Ширина, см
+                            <span class="description-and-specifications_specifications_item_line"></span>
+                            <span class="description-and-specifications_specifications_item_value">300</span>
+                        </p>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="container">
+
+    <section class="slider-wrapper-for-goods">
+        <h2 class="slider-wrapper-for-goods_header">Похожие товары</h2>
+        <div class="slider-wrapper-for-goods_swiper-wrapper">
+
+            <div class="swiper swiper-slider-wrapper-for-goods">
+                <div class="swiper-wrapper">
+                    <?php for ($j = 0; $j < 6; $j++) { ?>
+                        <div class="swiper-slide">
+                            <div class="slider-wrapper-for-goods_item">
+                                <p class="slider-wrapper-for-goods_item_status status-blue">Новинка</p>
+                                <img class="slider-wrapper-for-goods_item_img" src="images/image%20438.png" alt="">
+                                <div class="slider-wrapper-for-goods_item_line"></div>
+                                <div class="product-cards_grade-wrappers">
+                                    <div class="product-cards_grade-star-wrappers">
+                                        <div class="product-cards_grade-star is-active"></div>
+                                        <div class="product-cards_grade-star is-active"></div>
+                                        <div class="product-cards_grade-star is-active"></div>
+                                        <div class="product-cards_grade-star is-active"></div>
+                                        <div class="product-cards_grade-star"></div>
+                                        <p class="product-cards_grade-value">5.0</p>
+                                    </div>
+                                    <p class="product-cards_review-value"><span>56</span> отзывов</p>
+                                </div>
+                                <p class="slider-wrapper-for-goods_item_description">Профилированный монолитный
+                                    поликарбонатxvcxsd</p>
+                                <p class="slider-wrapper-for-goods_item_prices">500 руб.<span>790 руб.</span></p>
+                                <p class="product-cards_price-and-purchase_in-stock">В наличии</p>
+                                <button class="product-cards_price-and-purchase_add-to-basket btn-red">
+                                    <span class="pc-monitor">Добавить в корзину</span>
+                                    <span class="mobile-monitor">В корзину</span>
+                                </button>
+                                <button class="product-cards_price-and-purchase_Buy-in-a-click btn-grey">Купить в 1
+                                    клик
+                                </button>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+
+        </div>
+        <div class="slider-wrapper-for-goods_slider-btn-wrapper">
+            <div class="slider-wrapper-for-goods_prev"></div>
+            <div class="slider-wrapper-for-goods_next"></div>
+        </div>
+    </section>
+
+    <section class="slider-wrapper-for-goods">
+        <h2 class="slider-wrapper-for-goods_header">Сопутствующие товары</h2>
+        <div class="slider-wrapper-for-goods_swiper-wrapper">
+
+            <div class="swiper swiper-slider-wrapper-for-goods">
+                <div class="swiper-wrapper">
+                    <?php for ($j = 0; $j < 6; $j++) { ?>
+                        <div class="swiper-slide">
+                            <div class="slider-wrapper-for-goods_item">
+                                <p class="slider-wrapper-for-goods_item_status status-yellow">Хит</p>
+                                <img class="slider-wrapper-for-goods_item_img" src="images/image%20438.png" alt="">
+                                <div class="slider-wrapper-for-goods_item_line"></div>
+                                <div class="product-cards_grade-wrappers">
+                                    <div class="product-cards_grade-star-wrappers">
+                                        <div class="product-cards_grade-star is-active"></div>
+                                        <div class="product-cards_grade-star is-active"></div>
+                                        <div class="product-cards_grade-star is-active"></div>
+                                        <div class="product-cards_grade-star is-active"></div>
+                                        <div class="product-cards_grade-star"></div>
+                                        <p class="product-cards_grade-value">5.0</p>
+                                    </div>
+                                    <p class="product-cards_review-value"><span>56</span> отзывов</p>
+                                </div>
+                                <p class="slider-wrapper-for-goods_item_description">Профилированный монолитный
+                                    поликарбонатxvcxsd</p>
+                                <p class="slider-wrapper-for-goods_item_prices">500 руб.<span>790 руб.</span></p>
+                                <p class="product-cards_price-and-purchase_in-stock">В наличии</p>
+                                <button class="product-cards_price-and-purchase_add-to-basket btn-red">
+                                    <span class="pc-monitor">Добавить в корзину</span>
+                                    <span class="mobile-monitor">В корзину</span>
+                                </button>
+                                <button class="product-cards_price-and-purchase_Buy-in-a-click btn-grey">Купить в 1
+                                    клик
+                                </button>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+
+        </div>
+        <div class="slider-wrapper-for-goods_slider-btn-wrapper">
+            <div class="slider-wrapper-for-goods_prev"></div>
+            <div class="slider-wrapper-for-goods_next"></div>
+        </div>
+    </section>
+
+    <section class="slider-wrapper-for-goods">
+        <h2 class="slider-wrapper-for-goods_header">Акции</h2>
+        <div class="slider-wrapper-for-goods_swiper-wrapper">
+
+            <div class="swiper swiper-slider-wrapper-for-goods">
+                <div class="swiper-wrapper">
+                    <?php for ($j = 0; $j < 6; $j++) { ?>
+                        <div class="swiper-slide">
+                            <div class="slider-wrapper-for-goods_item">
+                                <p class="slider-wrapper-for-goods_item_status status-greenery">Скидка</p>
+                                <img class="slider-wrapper-for-goods_item_img" src="images/image%20438.png" alt="">
+                                <div class="slider-wrapper-for-goods_item_line"></div>
+                                <div class="product-cards_grade-wrappers">
+                                    <div class="product-cards_grade-star-wrappers">
+                                        <div class="product-cards_grade-star is-active"></div>
+                                        <div class="product-cards_grade-star is-active"></div>
+                                        <div class="product-cards_grade-star is-active"></div>
+                                        <div class="product-cards_grade-star is-active"></div>
+                                        <div class="product-cards_grade-star"></div>
+                                        <p class="product-cards_grade-value">5.0</p>
+                                    </div>
+                                    <p class="product-cards_review-value"><span>56</span> отзывов</p>
+                                </div>
+                                <p class="slider-wrapper-for-goods_item_description">Профилированный монолитный
+                                    поликарбонатxvcxsd</p>
+                                <p class="slider-wrapper-for-goods_item_prices">500 руб.<span>790 руб.</span></p>
+                                <p class="product-cards_price-and-purchase_in-stock">В наличии</p>
+                                <button class="product-cards_price-and-purchase_add-to-basket btn-red">
+                                    <span class="pc-monitor">Добавить в корзину</span>
+                                    <span class="mobile-monitor">В корзину</span>
+                                </button>
+                                <button class="product-cards_price-and-purchase_Buy-in-a-click btn-grey">Купить в 1
+                                    клик
+                                </button>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+
+        </div>
+        <div class="slider-wrapper-for-goods_slider-btn-wrapper">
+            <div class="slider-wrapper-for-goods_prev"></div>
+            <div class="slider-wrapper-for-goods_next"></div>
+        </div>
+    </section>
+
+</div>
 
 <script src="scripts.js"></script>
 </body>
--- scripts.js
+++ scripts.js
@@ -21,3 +21,58 @@ document.addEventListener('click', (e) => {
         btn.classList.toggle('close')
     }
 })
+
+document.addEventListener('click', (e) => {
+    const toggleDescription = e.target.closest('[data-description-and-specifications-toggle-description]');
+    const toggleSpecifications = e.target.closest('[data-description-and-specifications-toggle-specifications]');
+
+
+    if (toggleDescription) {
+        const wrapper = e.target.closest('[data-description-and-specifications-wrapper]');
+        const specifications = wrapper.querySelector('[data-description-and-specifications-specifications]');
+        const description = wrapper.querySelector('[data-description-and-specifications-description]');
+        const oldAssetButton = wrapper.querySelector('[data-description-and-specifications-toggle-specifications] ').querySelector('p');
+
+        description.classList.add('active')
+        specifications.classList.remove('active')
+        toggleDescription.querySelector('p').classList.add('active')
+        oldAssetButton.classList.remove('active')
+    }
+    if (toggleSpecifications) {
+        const wrapper = e.target.closest('[data-description-and-specifications-wrapper]');
+        const specifications = wrapper.querySelector('[data-description-and-specifications-specifications]');
+        const description = wrapper.querySelector('[data-description-and-specifications-description]');
+        const oldAssetButton = wrapper.querySelector('[data-description-and-specifications-toggle-description]').querySelector('p');
+
+
+        specifications.classList.add('active')
+        description.classList.remove('active')
+        oldAssetButton.classList.remove('active')
+        toggleSpecifications.querySelector('p').classList.add('active')
+    }
+})
+
+document.querySelectorAll('.swiper-slider-wrapper-for-goods').forEach((e, index) => {
+    new Swiper( document.querySelectorAll('.swiper-slider-wrapper-for-goods')[index], {
+        navigation: {
+            nextEl: document.querySelectorAll('.slider-wrapper-for-goods_next')[index],
+            prevEl: document.querySelectorAll('.slider-wrapper-for-goods_prev')[index],
+        },
+        slidesPerView: 1,
+        spaceBetween: 10,
+        breakpoints: {
+            320: {
+                slidesPerView: 2,
+                spaceBetween: 10
+            },
+            690: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            },
+            992: {
+                slidesPerView: 4,
+                spaceBetween: 30
+            }
+        }
+    });
+})
